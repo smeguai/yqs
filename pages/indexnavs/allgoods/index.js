5ae78e632f0170820da62fab311c1c6ac24f468f
smeguai <smeguail@163.com>
2019-09-11 04:36:27 +0800

    asd

--- pages/indexnavs/allgoods/index.js
+++ pages/indexnavs/allgoods/index.js
@@ -43,8 +43,10 @@ Page({
     x: null,
     y: null,
     location: null,
-    shoptypeLs: null,
-    shoptypeIdx: 0
+    shoptypeLs: [],
+    shoptypeIdx: 0,
+    totalCount: null,
+    shopMoreLS: true
   },
 
   /**
@@ -61,65 +63,90 @@ Page({
   },
   //  获取navbar
   getNavBar() {
-    let station = wx.getStorageSync('station')
-    promiseRequest(collnavbar, 'get', {
-      stationId: this.data.stationId,
-      type: this.data.navIdx
-    }).then(res => {
-      if (res.data.code == 0) {
-        this.setData({
-          shoptypeLs: res.data.data
-        })
-        let i = parseInt(this.data.navIdx)
-        switch (i) {
-          case 3:
-            this.getCutList()
-            break;
-          case 2:
-            this.getLimitList()
-            break;
-          case 1:
-            this.getGroupList()
-            break;
-          case 0:
-            this.getOrdinaryList()
-            break;
-        }
+    let arr = [{
+        productTypeName: '女装',
+        productTypeId: 1,
+        recommend: '说点啥？'
+      },
+      {
+        productTypeName: '男装',
+        productTypeId: 2,
+        recommend: '这是备注'
+      },
+      {
+        productTypeName: '电子',
+        productTypeId: 3,
+        recommend: '快没了'
+      },
+      {
+        productTypeName: '瓶',
+        productTypeId: 4,
+        recommend: '没了熬'
       }
+    ]
+    this.setData({
+      shoptypeLs: arr
     })
+    let i = parseInt(this.data.navIdx)
+    switch (i) {
+      case 3:
+        this.getCutList()
+        break;
+      case 2:
+        this.getLimitList()
+        break;
+      case 1:
+        this.getGroupList()
+        break;
+      case 0:
+        this.getOrdinaryList()
+        break;
+    }
+    // let station = wx.getStorageSync('station')
+    // promiseRequest(collnavbar, 'get', {
+    //   stationId: this.data.stationId,
+    //   type: this.data.navIdx
+    // }).then(res => {
+    //   if (res.data.code == 0) {
+    //     this.setData({
+    //       shoptypeLs: res.data.data
+    //     })
+    // if (!res.data.data[this.data.shoptypeIdx]) return
+    //     let i = parseInt(this.data.navIdx)
+    //     switch (i) {
+    //       case 3:
+    //         this.getCutList()
+    //         break;
+    //       case 2:
+    //         this.getLimitList()
+    //         break;
+    //       case 1:
+    //         this.getGroupList()
+    //         break;
+    //       case 0:
+    //         this.getOrdinaryList()
+    //         break;
+    //     }
+    //   }
+    // })
   },
   //  跳转商品
   handleNavigate(e) {
-    this.data.list[id].map(item => {
-      if (item.list.productId == e.currentTarget.dataset.id) {
-        let pid = null
-        let name = null
-        switch (this.data.navIdx) {
-          case 3:
-            pid = item.list.productCutPriceId
-            name = 'cut'
-            break;
-          case 2:
-            pid = item.list.timeLimitBuyId
-            name = 'limit'
-            break;
-          case 1:
-            pid = item.list.productGroupBuyId
-            name = 'group'
-            break;
-          case 0:
-            pid = item.list.productId
-            name = 'product'
-            break;
-        }
-        wx.navigateTo({
-          url: `../../goodsdetail/index?pid=${pid}&name=${name}`
-        })
-      }
+    let pid = e.currentTarget.dataset.id
+    let name = e.currentTarget.dataset.name
+    wx.navigateTo({
+      url: `../../goodsdetail/index?pid=${pid}&name=${name}`
     })
   },
   //  get ordinary goods
   getOrdinaryList() {
+    let shopCurrent = this.data.shoptypeLs[this.data.shoptypeIdx]
+    if (!shopCurrent) {
+      this.setData({
+        notlist: true
+      })
+      return
+    }
     promiseRequest(getgoodslist, 'get', {
       pageIndex: this.data.pageIndex,
       pageSize: this.data.pageSize,
@@ -129,21 +156,48 @@ Page({
       y: this.data.location[1],
       typeId: 0,
       keys: '',
-      orderby: 0
+      orderby: this.data.shoptypeLs[this.data.shoptypeIdx].productTypeId
     }).then(res => {
-      if (res.data.code == 0) {
-        if (res.data.data.length < this.data.pageSize) {
-          let shopCurrent = this.data.shoptypeLs[this.data.shoptypeIdx]
-          console.log(shopCurrent)
-          this.setData({
-            shoptypeIdx: this.data.shoptypeIdx + 1,
-            list: [...this.data.list, {
-              title: shopCurrent && shopCurrent.productTypeName,
-              id: shopCurrent && shopCurrent.productTypeId,
-              recommend: shopCurrent && shopCurrent.recommend,
+      if (res.data.code == 0 && shopCurrent) {
+        if (res.data.data.length > 0) {
+          let list = null
+          let newlist = null
+          if (this.data.list.length > 0 && this.data.list[this.data.list.length - 1].list.length < this.data.totalCount) {
+            list = `list[${this.data.list.length - 1}].list`
+            newlist = this.data.list[this.data.list.length - 1].list.concat(res.data.data)
+          } else {
+            list = 'list'
+            newlist = [...this.data.list, {
+              title: shopCurrent.productTypeName,
+              id: shopCurrent.productTypeId,
+              recommend: shopCurrent.recommend,
               list: res.data.data
             }]
+          }
+          this.setData({
+            [list]: newlist,
+            totalCount: res.data.totalCount
           })
+          if (this.data.shopMoreLS) {
+            let arr = []
+            this.data.list.map(item => {
+              arr.push(...item.list)
+            })
+            console.log(this.data.list)
+            if (arr.length < this.data.pageSize) {
+              this.setData({
+                shoptypeIdx: this.data.shoptypeIdx + 1,
+                pageIndex: 1
+              })
+              this.getOrdinaryList()
+            }
+          }
+        } else {
+          this.setData({
+            shoptypeIdx: this.data.shoptypeIdx + 1,
+            pageIndex: 1
+          })
+          this.getOrdinaryList()
         }
       }
     })
@@ -178,36 +232,38 @@ Page({
   },
   //  get limit goods
   getLimitList() {
-    promiseRequest(limit, 'get', {
-      pageIndex: this.data.pageIndex,
-      pageSize: this.data.pageSize,
-      stationId: this.data.stationId,
-      merchantId: this.data.merchantId,
-      keys: '',
-      orderby: 0,
-      x: this.data.location[0],
-      y: this.data.location[1],
-      searchType: 0
-    }).then(res => {
-      if (res.data.code == 0) {
-        if (res.data.data.length < this.data.pageSize) {
-          let shopCurrent = this.data.shoptypeLs[this.data.shoptypeIdx]
-          this.setData({
-            shoptypeIdx: this.data.shoptypeIdx + 1,
-            list: [...this.data.list, {
-              title: shopCurrent.productTypeName,
-              id: shopCurrent.productTypeId,
-              recommend: shopCurrent.recommend,
-              list: res.data.data
-            }]
-          })
+    if (this.data.shoptypeLs) {
+      promiseRequest(limit, 'get', {
+        pageIndex: this.data.pageIndex,
+        pageSize: this.data.pageSize,
+        stationId: this.data.stationId,
+        merchantId: this.data.merchantId,
+        keys: '',
+        orderby: this.data.shoptypeLs[this.data.shoptypeIdx].productTypeId,
+        x: this.data.location[0],
+        y: this.data.location[1],
+        searchType: 0
+      }).then(res => {
+        if (res.data.code == 0) {
+          if (res.data.data.length < this.data.pageSize) {
+            let shopCurrent = this.data.shoptypeLs[this.data.shoptypeIdx]
+            this.setData({
+              shoptypeIdx: this.data.shoptypeIdx + 1,
+              list: [...this.data.list, {
+                title: shopCurrent.productTypeName,
+                id: shopCurrent.productTypeId,
+                recommend: shopCurrent.recommend,
+                list: res.data.data
+              }]
+            })
+          }
         }
-      }
-    })
+      })
+    }
   },
   //  get cut goods
   getCutList() {
-    console.log(this.data.shoptypeIdx)
+    console.log(this.data.shoptypeLs[this.data.shoptypeIdx])
     if (this.data.shoptypeLs) {
       promiseRequest(cut, 'get', {
         pageIndex: this.data.pageIndex,
@@ -230,11 +286,6 @@ Page({
                 recommend: shopCurrent.recommend,
                 list: res.data.data
               }]
-            })
-          } else {
-            let list = this.data.list[this.data.list.length - 1].list
-            this.setData({
-              [list]: list.push(res.data.data)
             })
           }
           console.log(this.data.list)
@@ -268,9 +319,26 @@ Page({
       notlist: false,
       shoptypeIdx: 0
     })
+    this.getNavBar()
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+    let shopCurrent = this.data.shoptypeLs[this.data.shoptypeIdx]
+    if (!shopCurrent) {
+      this.setData({
+        notlist: true
+      })
+      return
+    }
+    this.setData({
+      pageIndex: this.data.pageIndex + 1
+    })
     let i = parseInt(this.data.navIdx)
     switch (i) {
-      case 3:
+      case 33:
         this.getCutList()
         break;
       case 2:
@@ -282,40 +350,6 @@ Page({
       case 0:
         this.getOrdinaryList()
         break;
-    }
-    setTimeout(() => {
-      console.log(this.data.list)
-    }, 2000)
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function() {
-    if (this.data.list.length < this.data.pageIndex * this.data.pageSize && this.data.shoptypeLs.length > this.data.shoptypeIdx) {
-      this.setData({
-        notlist: true,
-        shoptypeIdx: this.data.shoptypeIdx + 1
-      })
-    } else {
-      this.setData({
-        pageIndex: this.data.pageIndex + 1
-      })
-      let i = parseInt(this.data.navIdx)
-      switch (i) {
-        case 0:
-          this.getCutList()
-          break;
-        case 1:
-          this.getLimitList()
-          break;
-        case 2:
-          this.getGroupList()
-          break;
-        case 3:
-          this.getOrdinaryList()
-          break;
-      }
     }
   }
 })
